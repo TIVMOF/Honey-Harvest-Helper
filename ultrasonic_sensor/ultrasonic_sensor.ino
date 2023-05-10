dedcad7028cd3c64d1ce449543609f8b37250910
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2023-05-10 18:40:33 +0300

    Update ultrasonic_sensor.ino
    including the nRF24L01 code

--- ultrasonic_sensor/ultrasonic_sensor.ino
+++ ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,12 +1,34 @@
+// Libraries for the nRF24L01
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// Defining the nRF24L01 pins
+// It also uses pins 11, 12, 13 for SPI
+RF24 radio(7, 8); // CE, CSN
+
+// The address with which the nRF24L01s recognises the other
+const byte address[6] = "00001";
+
 // Defining the ultrasonic sensor pins
 const int triggerPin = 2;
 const int echoPin = 3;
 
-// Hight at withc the sensor trigers
-int triggerHight = 2;
+// Hight at which the sensor trigers
+int triggerHight = 4;
 
-void setup(){
-  Serial.begin(9600);
+void setup() {
+  // Starting the nRF24L01
+  radio.begin();
+
+  // Defining a chanel to send info to
+  radio.openWritingPipe(address);
+
+  // Setting the power level - distance in which the nRF24L01s work
+  radio.setPALevel(RF24_PA_MIN);
+
+  // nRF24L01 stops listening and prepares to write
+  radio.stopListening();
 }
 
 long microsecondsToCentimeters(long microseconds){
@@ -16,7 +38,6 @@ long microsecondsToCentimeters(long microseconds){
 
 void loop(){
   long duration;
-  long inches;
   long cm;
 
   // Next section sends signals in every 10 microsecond
@@ -38,10 +59,12 @@ void loop(){
   // because the distace = traveling to + going back
   cm = microsecondsToCentimeters(duration) / 2;
 
-  if(cm <= 4){
-    Serial.print(cm);
+  // When water level is close eneogh
+  // a message is passed to the main part
+  if(cm <= triggerHight){
+    const char text[] = "Hello World";
+    radio.write(&text, sizeof(text));
   }
-  Serial.print("\n");
 
   delay(1000);
 }
